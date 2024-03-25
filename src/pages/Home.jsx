0dead3ea59d5662c16d6bce004d4af8943711a99
Bormach_Boroda <vano888888@gmail.com>
2024-03-26 00:04:27 +0300

    chore: not essential changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import { Card } from "../components/Card";
+import { getCards } from "../../api/cards";
 
-export function Home() {
-  return <div>Home</div>;
+function CardsList() {
+  const cards = useLoaderData();
+  return (
+    <div className="cards-list">
+      {cards.map((card) => {
+        const { cardId, title, desc, icon, path } = card;
+        return (
+          <Link key={cardId} to={path}>
+            <Card title={title} desc={desc} icon={icon} />
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
+
+async function loader({ request: { signal } }) {
+  return await getCards({ signal });
+}
+
+export const homeRoute = {
+  element: <CardsList />,
+  loader,
+};
--- src/router.jsx
+++ src/router.jsx
@@ -1,23 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import { RootLayout } from "./layouts/RootLayout";
-import { cardsListRoute } from "./components/CardsList";
-import { contentsRoute } from "./components/Contents";
-import { notesRoute } from "./components/Notes";
+import { homeRoute } from "./pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        index: true,
-        ...cardsListRoute,
-      },
-      {
-        path: "react",
-        ...contentsRoute,
-        children: [{ index: true, ...notesRoute }],
-      },
-    ],
+    ...homeRoute,
+    children: [],
   },
 ]);
